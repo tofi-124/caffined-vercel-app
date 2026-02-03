0b563de1a58634a9d011023b72034540aae66c9d
tofi-124 <tofikahm@yahoo.com>
2026-02-02 23:37:37 -0500

    feat: Add favicon and apple touch icon for improved branding and user experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,7 +56,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
+        <link rel="icon" href="/icon.png" type="image/png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/apple-icon.png" sizes="180x180" />
         <Script
           id="structured-data"
           type="application/ld+json"
