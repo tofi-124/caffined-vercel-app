c67fc035aa9d50d8fd4ba35eb78620e1b97e0d9b
tofi-124 <tofikahm@yahoo.com>
2026-02-02 23:41:52 -0500

    fix: Correctly close head tag and adjust body structure in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,8 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" sizes="32x32" />
         <link rel="icon" href="/icon.png" type="image/png" sizes="32x32" />
         <link rel="apple-touch-icon" href="/apple-icon.png" sizes="180x180" />
+      </head>
+      <body className={`${oswald.className} bg-primary`}>
         <Script
           id="structured-data"
           type="application/ld+json"
@@ -218,8 +220,6 @@ export default function RootLayout({
             })
           }}
         />
-      </head>
-      <body className={`${oswald.className} bg-primary`}>
         <ScrollToTop />
         <Navbar />
         {children}
