c94d823dd912ebc73f515f9262cfd8e63ef6911c
tofi-124 <tofikahm@yahoo.com>
2026-03-01 02:43:53 -0500

    feat: refactor country data management by moving country list to a separate module

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -20,41 +20,7 @@ const CheckoutPayPal = dynamic(() => import('../components/CheckoutPayPal'), { s
 
 export { type ShippingAddress } from '../components/CheckoutPayPal'
 
-const COUNTRIES = [
-  { code: 'US', name: 'United States' },
-  { code: 'CA', name: 'Canada' },
-  { code: 'GB', name: 'United Kingdom' },
-  { code: 'DE', name: 'Germany' },
-  { code: 'FR', name: 'France' },
-  { code: 'NL', name: 'Netherlands' },
-  { code: 'BE', name: 'Belgium' },
-  { code: 'IT', name: 'Italy' },
-  { code: 'ES', name: 'Spain' },
-  { code: 'SE', name: 'Sweden' },
-  { code: 'NO', name: 'Norway' },
-  { code: 'DK', name: 'Denmark' },
-  { code: 'FI', name: 'Finland' },
-  { code: 'CH', name: 'Switzerland' },
-  { code: 'AT', name: 'Austria' },
-  { code: 'AU', name: 'Australia' },
-  { code: 'NZ', name: 'New Zealand' },
-  { code: 'JP', name: 'Japan' },
-  { code: 'KR', name: 'South Korea' },
-  { code: 'CN', name: 'China' },
-  { code: 'SG', name: 'Singapore' },
-  { code: 'AE', name: 'United Arab Emirates' },
-  { code: 'SA', name: 'Saudi Arabia' },
-  { code: 'IN', name: 'India' },
-  { code: 'BR', name: 'Brazil' },
-  { code: 'MX', name: 'Mexico' },
-  { code: 'ZA', name: 'South Africa' },
-  { code: 'NG', name: 'Nigeria' },
-  { code: 'KE', name: 'Kenya' },
-  { code: 'TR', name: 'Turkey' },
-  { code: 'PL', name: 'Poland' },
-  { code: 'CZ', name: 'Czech Republic' },
-  { code: 'RO', name: 'Romania' },
-]
+import { COUNTRIES } from '../data/countries'
 
 export default function CheckoutPage() {
   const { items, removeItem, updateQuantity, itemCount, subtotal, totalWeightGrams, clearCart } = useCart()
