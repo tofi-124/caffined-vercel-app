3de13a97241282e7c53cb696dfc0891eec05a050
tofi-124 <tofikahm@yahoo.com>
2026-04-04 11:39:08 -0400

    feat: Add Microsoft Clarity tracking script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -89,6 +89,15 @@ export default function RootLayout({
             gtag('config', 'G-7XP6DNBM6J');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "w6hpgb5sdh");
+          `}
+        </Script>
       </head>
       <body className={`${oswald.className} ${inconsolata.variable} bg-primary`}>
         <Script
